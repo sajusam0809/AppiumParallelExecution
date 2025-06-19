93d44efd4eeb660627dfe79f3f4bf22d9d94b303
saju <sajusam0809@gmail.com>
2025-06-19 15:25:14 -0400

    AppiumServerManagerClass

--- src/main/java/org/appium/utilis/AppiumServerManager.java
+++ src/main/java/org/appium/utilis/AppiumServerManager.java
@@ -9,14 +9,26 @@ public class AppiumServerManager {
     private AppiumDriverLocalService service;
 
     public void startServer(int port) {
-        service = new AppiumServiceBuilder()
+/*        service = new AppiumServiceBuilder()
                 .usingPort(port)
                 .withAppiumJS(new File("C://Users//JenkinsUser//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
                 .build();
         service.start();
         System.out.println("Appium server started on port: " + port);
-    }
+    }*/
 
+        File nodeExecutable = new File("C://Program Files//nodejs//node.exe");
+        // ✅ Full path to Appium main.js
+        File appiumMainJs = new File("C://Users//JenkinsUser//AppData//Roaming//npm//node_modules//appium//build//lib//main.js");
+
+        AppiumServiceBuilder builder = new AppiumServiceBuilder()
+                .usingDriverExecutable(nodeExecutable)
+                .withAppiumJS(appiumMainJs)
+                .usingPort(port);
+
+        service = AppiumDriverLocalService.buildService(builder);
+        service.start();
+    }
     public void stopServer() {
         if (service != null && service.isRunning()) {
             service.stop();
